7217bcbd4aae581adc23f179dbbfa28c004e05a2
Aleskey <sav459811@mail.ru>
2022-01-02 11:22:00 +0500

    Glad add

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -35,6 +35,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/SampleGit/build/external/glfw/cmake_install.cmake")
+  include("C:/SampleGit/build/external/glad/cmake_install.cmake")
 
 endif()
 
